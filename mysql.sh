8c599b473450bdee4d50f4e4c06699dd0b9bbd45
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-04-19 14:14:48 -0700

    created shell script for frontend

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 dnf module disable mysql -y
-cp /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
