e0bcacfedad6a43147d6b82f028541d8cdee5624
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-04-19 14:31:54 -0700

    created shell script for frontend

--- backend.sh
+++ backend.sh
@@ -4,7 +4,9 @@ dnf install nodejs -y
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+pwd
 cd /app
+pwd
 unzip /tmp/backend.zip
 npm install
 cp backend.service /etc/systemd/system/backend.service
