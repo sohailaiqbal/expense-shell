7b82017534451691b28b36751661bb23c9a5c6e5
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-04-19 14:34:54 -0700

    created shell script for frontend

--- backend.sh
+++ backend.sh
@@ -1,15 +1,15 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 pwd
 cd /app
-pwd
 unzip /tmp/backend.zip
 npm install
-cp backend.service /etc/systemd/system/backend.service
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
