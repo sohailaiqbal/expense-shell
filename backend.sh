55c1e9c67fffbd1de36b4216971aa8ec446d61d5
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-04-19 14:36:52 -0700

    created shell script for frontend

--- backend.sh
+++ backend.sh
@@ -3,6 +3,7 @@ dnf module enable nodejs:18 -y
 dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
 useradd expense
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 pwd
