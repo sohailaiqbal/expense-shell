711756f8bb3ca1868f2c66d78f038d7fbb63b686
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-04-19 13:54:08 -0700

    created shell script for frontend

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,9 @@
 dnf install nginx -y
-systemctl enable nginx
+cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+systemctl enable nginx
 systemctl restart nginx
