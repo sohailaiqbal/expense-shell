2aa11f970454d68898efc60b089586aa3d194408
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-05-01 14:15:45 -0700

    created shell script for frontend

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,35 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+Head() {
+  echo -e "\e[36m$1\e[0m"
+  }
+
+Head "INSTALLING NGINX"
+dnf install nginx -y &>>/tmp/expense.log
+echo $?
+
+Head "XPENSE CONFIG FILE"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+echo $?
+
+Head "Loading NGINX"
+systemctl start nginx &>>/tmp/expense.log
+echo $?
+
+Head "REMOVING  OLD/DEFAULT CONTENT"
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+echo $?
+
+Head "DOWNLOADING APPLICATION CODE"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+echo $?
+
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+
+Head "EXTRACTING APPLICATION CODE"
+unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo $?
+
+Head  "STARTING NGINX SERVICE"
+systemctl enable nginx &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
+echo $?
