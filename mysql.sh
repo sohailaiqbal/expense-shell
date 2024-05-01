8dc7f3b14457f11e656037044bc9b0768584e085
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-05-01 15:03:52 -0700

    created shell script for frontend

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,23 @@
 MYSQL_PASSWORD=$1
+source common.sh
+
+Head "DISABLING OLC MYSQL VERSION"
 dnf module disable mysql -y
+echo $?
+
+Head "MYSQL REPO.FILE"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo $?
+
+Head "INSTALLING MYSQL"
 dnf install mysql-community-server -y
+echo $?
+
+Head "ENABLING & STARTING MYSQL SERVICES"
 systemctl enable mysqld
 systemctl start mysqld
+echo $?
+
+Head "SETTING DB PASSWORD"
 mysql_secure_installation --set-root-pass ${MYSQL_PASSWORD}
+echo $?
