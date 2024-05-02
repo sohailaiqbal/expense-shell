13279708e8a368a8c548f1cf526be3a80c89ca69
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-05-02 16:01:19 -0700

    created shell script for frontend

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,5 @@
-source common.sh
 component=frontend
+source common.sh
 
 Head "INSTALLING NGINX"
 dnf install nginx -y &>>/tmp/expense.log
@@ -9,20 +9,7 @@ Head "XPENSE CONFIG FILE"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 echo $?
 
-Head "REMOVING OLD/DEFAULT CONTENT"
-rm -rf /usr/share/nginx/html/*
-echo $?
-
-Head "DOWNLOADING APPLICATION CODE"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
-echo $?
-
-cd /usr/share/nginx/html
-
-
-Head "EXTRACTING APPLICATION CODE"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
-echo $?
+App_Prereq "/usr/share/nginix/html"
 
 Head  "STARTING NGINX SERVICE"
 systemctl enable nginx &>>/tmp/expense.log
