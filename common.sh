b737536de6831d1891ad740122f1ea7dc3163fc6
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-05-02 15:35:30 -0700

    created shell script for frontend

--- common.sh
+++ common.sh
@@ -3,3 +3,25 @@ log_file=/tmp/expense.log
 Head() {
   echo -e "\e[35m$1\e[0m"
 }
+
+App_Prereq() {
+  DIR=$1
+
+  Head "Removing Existing App Content"
+  rm -rf $1 &>>/tmp/expense.log
+  echo $?
+
+  Head "CREATING APPLICATION DIRECTORY"
+  mkdir $1 &>>/tmp/expense.log
+  echo $?
+
+  Head "DOWNLOADING APPLICATION CONTENT"
+  curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>/tmp/expense.log
+  echo $?
+
+  cd $1
+
+  Head "EXTRACTING APPLICATION CONTENT"
+  unzip /tmp/backend.zip &>>/tmp/expense.log
+  echo $?
+}
